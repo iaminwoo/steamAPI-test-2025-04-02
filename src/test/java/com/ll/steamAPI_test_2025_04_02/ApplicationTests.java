5444d80a27c081f25c6cef0bb22b3b850e7d0849
inwoo <dlsdn1108@gmail.com>
2025-04-02 15:38:38 +0900

    init

---
+++ src/test/java/com/ll/steamAPI_test_2025_04_02/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.steamAPI_test_2025_04_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
